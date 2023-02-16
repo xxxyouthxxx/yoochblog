a73d734bba670d5ba30ac2fa1e220e0e54da2883
xxxyouthxxx <1042151752@qq.com>
2023-02-16 15:28:06 +0800

    设置了背景

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
@@ -22,5 +22,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body{
+  background-image: url(./assets/bg_trees.png);
 }
 </style>
