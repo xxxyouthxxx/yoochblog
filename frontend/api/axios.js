e024adc632a6490e6b1086beceb9bdea930d5cc0
xxxyouthxxx <1042151752@qq.com>
2023-02-20 11:02:47 +0800

    架构前后端以及后台管理页

---
+++ frontend/api/axios.js
@@ -0,0 +1,42 @@
+export default function({ $axios, redirect }) {
+    // $axios.defaults.baseURL = "https://api.qblog.cc/index"
+    $axios.defaults.baseURL = "http://127.0.0.1:9249"
+
+    $axios.onRequest(config => {
+        if (config.method == 'post') {
+            if (!config.data) {
+                config.data = {}
+            }
+        }
+        return config
+    }, function(error) {
+        return Promise.reject(error)
+    })
+
+    $axios.onResponse(res => {
+        switch (res.data.ret) {
+            case 200:
+                return res.data
+                    // case 401:
+                    //     Message({
+                    //         type: 'warning',
+                    //         message: res.data.msg,
+                    //         center: true,
+                    //         onClose: () => {
+                    //             localStorage.removeItem("access_token");
+                    //             location.reload();
+                    //         },
+                    //     })
+                    //     return false
+                    // default:
+                    //     Message({
+                    //         type: 'error',
+                    //         message: res.data.msg,
+                    //         center: true
+                    //     })
+                    //     return false
+        }
+    }, function(error) {
+        return Promise.reject(error)
+    })
+}
